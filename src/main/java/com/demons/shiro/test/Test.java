7940e977ed6041328fdf4552de0f6ec4b9243af6
20296 <20296@DESKTOP-INK7E08>
2020-04-08 15:51:28 +0800

    测试STS提交

---
+++ src/main/java/com/demons/shiro/test/Test.java
@@ -0,0 +1,11 @@
+package com.demons.shiro.test;
+/**
+*@author:liuhao
+*@version:2020年4月8日下午3:49:12
+*@description:
+*/
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("#2");
+	}
+}
