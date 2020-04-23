214abb334459eb5b8db342239e962bb15c185b8c
lh <useremail@qq.com>
2020-04-23 17:41:06 +0800

    lh

--- src/main/java/com/demons/shiro/test/Test.java
+++ src/main/java/com/demons/shiro/test/Test.java
@@ -1,11 +1,28 @@
 package com.demons.shiro.test;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 /**
 *@author:liuhao
 *@version:2020年4月8日下午3:49:12
 *@description:
 */
 public class Test {
+	
+	@PostConstruct
+	void started() {
+		System.out.println("123");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("#2");
+		System.out.println("args:"+args.toString());
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+		System.out.println(args.length);
+
 	}
 }
